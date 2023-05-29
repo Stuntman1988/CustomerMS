5e1d8569cbe77f9e431368862130c7fce1c4115b
Robin lundell <robin.lundell@hotmail.com>
2023-05-29 11:40:06 +0200

    added validation

--- src/main/java/com/example/customerms/Controller/CustomerController.java
+++ src/main/java/com/example/customerms/Controller/CustomerController.java
@@ -1,9 +1,14 @@
 package com.example.customerms.Controller;
 import com.example.customerms.Model.Customer;
 import com.example.customerms.Service.CustomerService;
+import jakarta.validation.Valid;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("customers")
@@ -27,9 +32,12 @@ public class CustomerController {
         return customerService.getCustomerById(id);
     }
 
-
     @PostMapping("/add")
-    public String addCustomer(@RequestBody Customer customer){
+    public String addCustomer(@RequestBody @Valid Customer customer, BindingResult bndResult){
+        if (bndResult.hasErrors()){
+            return ResponseEntity.badRequest().body(bndResult.getAllErrors().stream()
+                    .map(ObjectError::getDefaultMessage).collect(Collectors.joining())).getBody();
+        }
         return customerService.addCustomer(customer);
     }
 }
--- src/main/java/com/example/customerms/Model/Customer.java
+++ src/main/java/com/example/customerms/Model/Customer.java
@@ -1,6 +1,8 @@
 package com.example.customerms.Model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -14,7 +16,13 @@ public class Customer {
     @Id
     @GeneratedValue
     private long id;
+
+    @NotEmpty(message = "\"ssn\" can not be empty!\n")
+    @Size(min = 8, max = 10, message = "\"ssn\" must be between 8 and 10 characters.\n")
     private String ssn;
+
+    @NotEmpty(message = "\"name\" can not be empty!\n")
+    @Size(min = 2, max = 20, message = "\"name\" must be between 2 and 20 characters.\n")
     private String name;
 
     public Customer(String ssn, String name) {
